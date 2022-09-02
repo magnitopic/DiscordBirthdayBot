c67838e7ac90a7207b4ec34b9e8bc8433075ef8f
magnitopic <magnitopic@gmail.com>
2022-09-02 11:07:38 +0200

    New way of dealing with env vars

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,20 +1,21 @@
-const { SlashCommandBuilder, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 const { REST } = require('@discordjs/rest');
-const dotenv = require('dotenv').config();
+const { Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
 
-const clientId = process.env.clientId
-const guildId = process.env.guildId
-const token = process.env.DISCORD_TOKEN
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '10' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
